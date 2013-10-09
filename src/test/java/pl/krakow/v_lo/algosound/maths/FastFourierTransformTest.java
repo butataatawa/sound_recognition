81da4efeae6bfb27246d566b22ea884a25143bc3
ArcCha <adam1piekarczyk@gmail.com>
2013-10-10 00:53:47 +0200

    Add: Reading tests from file. Forward and backward test. Test of transform result using my implementation in cpp.

--- src/test/java/pl/krakow/v_lo/algosound/maths/FastFourierTransformTest.java
+++ src/test/java/pl/krakow/v_lo/algosound/maths/FastFourierTransformTest.java
@@ -5,8 +5,11 @@ package pl.krakow.v_lo.algosound.maths;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Scanner;
 import java.util.Vector;
 
 import org.apache.commons.math3.complex.Complex;
@@ -23,23 +26,51 @@ public class FastFourierTransformTest
 {
 
   Vector<Complex> inputData;
+  Vector<Complex> expectedOutput;
 
-  public FastFourierTransformTest(Vector<Complex> inputData)
+  public FastFourierTransformTest(Vector<Complex> inputData, Vector<Complex> expectedOutput)
   {
     this.inputData = inputData;
+    this.expectedOutput = expectedOutput;
   }
 
   @Parameters
   public static Collection<Object[]> generateData()
   {
-    Complex[] first = { new Complex(5), new Complex(4), new Complex(3) };
-    Complex[] second = { new Complex(9), new Complex(5), new Complex(8), new Complex(6), new Complex(7),
-        new Complex(3), new Complex(4), new Complex(1), new Complex(0) };
-
-    Object[][] data = new Object[][] { { new Vector<Complex>(Arrays.asList(first)) },
-        { new Vector<Complex>(Arrays.asList(second)) } };
-
-    return Arrays.asList(data);
+    InputStream in = FastFourierTransform.class.getClassLoader().getResourceAsStream("fftTests.txt");
+    Scanner scanner = new Scanner(in);
+    final int numberOfTestCases = scanner.nextInt();
+    scanner.nextLine();
+    Object[][] inputData = new Object[numberOfTestCases][2];
+    for(int i = 0; i < numberOfTestCases; ++i)
+    {
+      String line = scanner.nextLine();
+      String[] splitted = line.split("\\|"); 
+      
+      Vector<Complex> input = new Vector<Complex>();
+      Scanner inputScanner = new Scanner(splitted[0]);
+      while(inputScanner.hasNextDouble())
+      {
+        double real = inputScanner.nextDouble();
+        double imag = inputScanner.nextDouble();
+        input.add(new Complex(real, imag));
+      }
+      inputScanner.close();
+      inputData[i][0] = input;
+      
+      Vector<Complex> result = new Vector<Complex>();
+      Scanner resultScanner = new Scanner(splitted[1]);
+      while(resultScanner.hasNextDouble())
+      {
+        double real = resultScanner.nextDouble();
+        double imag = resultScanner.nextDouble();
+        result.add(new Complex(real, imag));
+      }
+      resultScanner.close();
+      inputData[i][1] = result;
+    }
+    scanner.close();
+    return Arrays.asList(inputData);
   }
 
   @Test
@@ -48,7 +79,20 @@ public class FastFourierTransformTest
     FastFourierTransform transform = new FastFourierTransform(inputData);
     transform.transformForward();
     transform.transformBackward();
-    assertEquals("Transforming forward and then backward should result in same vector", inputData,
+    assertEquals("Transforming forward and then backward should result in the same vector", inputData,
         transform.getResult());
+  }
+  
+  @Test
+  public void testResultOfTransform()
+  {
+    FastFourierTransform transform = new FastFourierTransform(inputData);
+    transform.transformForward();
+    Vector<Complex> result = transform.getResult();
+    for(int i = 0; i < result.size(); ++i)
+    {
+      assertEquals(expectedOutput.get(i).getReal(), result.get(i).getReal(), 10e-6);
+      assertEquals(expectedOutput.get(i).getImaginary(), result.get(i).getImaginary(), 10e-6);
+    }
   }
 }
