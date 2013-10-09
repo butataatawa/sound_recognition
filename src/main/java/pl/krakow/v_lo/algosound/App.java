ae341058ace377b1decfbce845e4e3d01d38f1ea
ArcCha <adam1piekarczyk@gmail.com>
2013-10-09 11:28:06 +0200

    Basic maven skeleton generated.

---
+++ src/main/java/pl/krakow/v_lo/algosound/App.java
@@ -0,0 +1,13 @@
+package pl.krakow.v_lo.algosound;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
